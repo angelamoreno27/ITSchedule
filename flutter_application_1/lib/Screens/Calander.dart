f7c0a9c2d5f7fb8839b3c2e6d4f2df1a30aa2f38
dearhart-abc <dylan.earhart01@utrgv.edu>
2022-09-09 14:35:45 -0500

    added calendar to CalendarScreen

--- flutter_application_1/lib/Screens/Calander.dart
+++ flutter_application_1/lib/Screens/Calander.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class CalanderScreen extends StatelessWidget {
+class CalanderScreen extends StatefulWidget {
   const CalanderScreen({super.key});
 
   @override
+  State<CalanderScreen> createState() => _CalanderScreenState();
+}
+
+class _CalanderScreenState extends State<CalanderScreen> {
+  DateTime? chosenDay;
+  DateTime? highlightedDay;
+  CalendarFormat calendarFormat = CalendarFormat.week;
+
+  @override
   Widget build(BuildContext context) {
-    return const Text("Calander");
+    return Column(children: <Widget>[
+      TableCalendar(
+        firstDay: DateTime.utc(2022, 5, 9),
+        lastDay: DateTime.utc(2023, 5, 9),
+        focusedDay: DateTime.now(),
+        calendarFormat: calendarFormat,
+        startingDayOfWeek: StartingDayOfWeek.sunday,
+        weekendDays: [DateTime.saturday, DateTime.sunday],
+        selectedDayPredicate: (day) {
+          if (day != DateTime.saturday && day != DateTime.sunday)
+            return isSameDay(chosenDay, day);
+          return false;
+        },
+        onDaySelected: (selectedDay, focusedDay) {
+          setState(() {
+            chosenDay = selectedDay;
+            highlightedDay = focusedDay;
+          });
+        },
+        onFormatChanged: (format) {
+          calendarFormat = format;
+        },
+        calendarStyle: CalendarStyle(
+          isTodayHighlighted: true,
+        ),
+        headerStyle: HeaderStyle(
+          titleCentered: true,
+          formatButtonVisible: false,
+        ),
+      )
+    ]);
   }
 }
